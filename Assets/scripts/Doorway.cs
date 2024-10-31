908da82441914e3d89e74beadf23b7ebd3e00480
LUKECONSTABLE <luke@lukeconstable.com>
2024-10-31 18:07:08 +1100

    transitions

--- Assets/scripts/Doorway.cs
+++ Assets/scripts/Doorway.cs
@@ -1,24 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.SceneManagement;
 
 public class Doorway : MonoBehaviour
 {
     [SerializeField] GameObject rotator;
     
+    Animator animator;
+    
     [SerializeField] float rotSpeed;
     [SerializeField] bool doorOpen;
     [SerializeField] string whereTo;
     [SerializeField] Color32 colour;
+    [SerializeField] float transitionDuration;
+    GameSession gs;
     //Material mat;
     //Renderer renderer = GetComponent<Renderer>();
     // Start is called before the first frame update
+   
     void Start()
     {
         //MeshRenderer renderer = transform.GetChild(0).GetChild(0).GetComponent<MeshRenderer>();
         //renderer.material.color = color;
-        
+        //ppvol = FindObjectOfType<Volume>();
+        //animator = ppvol.GetComponent<Animator>();
+        gs = FindObjectOfType<GameSession>();
+       
+
     }
 
     // Update is called once per frame
@@ -38,11 +51,19 @@ public class Doorway : MonoBehaviour
     void OnTriggerEnter(Collider other){
         if (other.CompareTag("Player")){
             if (doorOpen){
-                Debug.Log("entered doorway");
-                //var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-                SceneManager.LoadScene(whereTo);
+                //animator.SetTrigger("NextScene");
+                gs = FindObjectOfType<GameSession>();
+               gs.LeaveScene(whereTo);
+               
+                
             } 
         }
+    }
+ 
+   
+    public void BruteLoadScene(){
+         Debug.Log("entered doorway");
+                SceneManager.LoadScene(whereTo);
     }
    
 }
--- Assets/scripts/GameSession.cs
+++ Assets/scripts/GameSession.cs
@@ -1,15 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.Rendering;
 
 public class GameSession : MonoBehaviour
 {
     public HashSet<string> pickedUpItems = new();
+    [SerializeField] Volume ppvol;
+    public float transitionDuration = .5f;
+    [SerializeField] bool useSceneFade;
+    ColorAdjustments cAdjust;
     //Dictionary<string,bool> levelItemKeys = new Dictionary<string,bool>();
     void Awake()
     {
         Singleton();
+
+      
+
+    }
+    void Start(){
+        if(ppvol.profile.TryGet(out cAdjust)){
+            Debug.Log("found post effect");
+        } else {
+            Debug.Log("could not find effect");
+        }
     }
 
     private void Singleton()
@@ -18,10 +35,12 @@ public class GameSession : MonoBehaviour
 
         if (numGameSessions > 1)
         {
+            Debug.Log("game session exists, destroying lastest one");
             Destroy(gameObject);
         }
         else
         {
+            Debug.Log("keeping game session");
             DontDestroyOnLoad(gameObject);
         }
     }
@@ -39,6 +58,35 @@ public class GameSession : MonoBehaviour
     }
     public void ResetPickedUpItems(){
         pickedUpItems.Clear();
+    }
+    public void LeaveScene(string whereTo){
+        if(useSceneFade){
+            StartCoroutine(ILeaveScene(whereTo));
+    } else {
+        SceneManager.LoadScene(whereTo);
+    }
+    }
+
+    IEnumerator ILeaveScene(string whereTo){
+                Debug.Log("entered doorway");
+                float elapsed = 0f;
+                while (elapsed < transitionDuration){
+                    cAdjust.postExposure.value = Mathf.Lerp(0f,-6f, elapsed/transitionDuration);
+                    elapsed += Time.deltaTime;
+                    yield return null;
+                }
+                cAdjust.postExposure.value = -6;
+
+                SceneManager.LoadScene(whereTo);
+                elapsed = 0f;
+                while (elapsed < transitionDuration){
+                    cAdjust.postExposure.value = Mathf.Lerp(-6f,0f, elapsed/transitionDuration);
+                    elapsed += Time.deltaTime;
+                    yield return null;
+                }
+                cAdjust.postExposure.value = 0;
+
+                
     }
     
   
