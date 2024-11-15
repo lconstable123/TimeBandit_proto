f2d7fe6851ea08668ec464a8709f56f5e7f4b52e
LUKECONSTABLE <luke@lukeconstable.com>
2024-11-15 15:35:24 +1100

    slope traversal

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -2,20 +2,36 @@
 //using System.Collections;
 //using System.Collections.Generic;
 //using UnityEditor.Callbacks;
+
+using System;
+
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 //using UnityEngine.XR;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float playerSpeed;
-    [SerializeField] float groundDist;
+    [Header("locomotion")]
+    [SerializeField] float playerSpeed = 400000.0f;
+    [SerializeField] float groundDist = 1.5f;
     [SerializeField] float groundDrag;
+    [SerializeField] float playerHeight = 2f;
+    [SerializeField] float jumpPower = 4000000.0f;
+
+    [Header("Slope handling")]
+    public float maxSlopeAngle;
+    private RaycastHit slopeHit;
+    public bool OnSlopDebug;
+    public float SlopeAngleDebug;
+
+
     public LayerMask TerrainLayer;
     Rigidbody rb;
     SpriteRenderer sr;
     float x,y;
     Vector3 moveDir;
-    bool grounded;
+    public bool grounded;
 
     void Start()
     {
@@ -27,40 +43,84 @@ public class PlayerController : MonoBehaviour
     {
         x = Input.GetAxis("Horizontal");
         y = Input.GetAxis("Vertical");
-        moveDir = new Vector3(x,0,y);
-        grounded = Physics.Raycast(transform.position,Vector3.down,groundDist*0.5f+0.2f,TerrainLayer);
+        moveDir = new Vector3(x, 0, y);
+        
+        if (Input.GetKeyDown(KeyCode.Space)){
+        ProcessJump();
+        }
+        GroundProbe(true);
+        //rb.useGravity = !OnSlope();
+        void GroundProbe( bool debug)
+        {
+            Vector3 slopeChecker = new();
+            if (y > 0.3f)
+            {
+                slopeChecker = Vector3.down + (Vector3.forward * .3f);
+                grounded = Physics.Raycast(transform.position, slopeChecker, out slopeHit, groundDist * 0.5f + 0.2f, TerrainLayer);
 
-        if(grounded){
-            rb.drag = groundDrag;
-        } else {rb.drag = 0;};
-
-
+            }
+            else
+            {
+                slopeChecker = Vector3.down;
+                grounded = Physics.Raycast(transform.position, Vector3.down, out slopeHit, groundDist * 0.5f + 0.2f, TerrainLayer);
+            }
+            if (debug){Debug.DrawRay(transform.position, slopeChecker);}
+        }
     }
+
+    private void ProcessJump()
+    {
+        rb.AddForce(Vector3.up* jumpPower, ForceMode.Force);
+    }
+
     void FixedUpdate(){
         ProcessForce();
+        ProcessSpriteFlip();
     }
 
-
-    void ProcessVelocity(){
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
-        Vector3 moveDir = new Vector3(x,0,y);
-        //Debug.Log(moveDir);
-        rb.velocity = moveDir*playerSpeed;
+    private bool OnSlope(){
+        if(grounded){
+            float angle = Vector3.Angle(Vector3.up,slopeHit.normal);
+            OnSlopDebug = true;
+            SlopeAngleDebug = angle;
+            return angle < maxSlopeAngle && angle != 0;
+        }
+        OnSlopDebug = false;
         
-        if (x != 0 && x < 0){
-            sr.flipX=true;
-        }
-        else if (x != 0 && x > 0){
-            sr.flipX=false;
-
-        }
+        return false;
     }
+
+    private Vector3 getSlopeMoveDirection(){
+        return Vector3.ProjectOnPlane(moveDir,slopeHit.normal).normalized;
+    }
+
      void ProcessForce(){
-       
-        //Debug.Log(moveDir);
-        rb.AddForce(playerSpeed * Time.fixedDeltaTime * moveDir,ForceMode.Force);
-        
+       Vector3 force = new();
+
+        if(grounded){
+            //on ground
+            rb.drag = groundDrag;
+            if (OnSlope()){
+                rb.useGravity = false;
+                force = playerSpeed * Time.fixedDeltaTime * getSlopeMoveDirection(); 
+            } else {
+                rb.useGravity = true;
+                force = playerSpeed * Time.fixedDeltaTime * moveDir;
+            }
+        } else {
+            //in air
+                rb.useGravity= true;
+                rb.drag = 0;
+                force = playerSpeed*.1f * Time.fixedDeltaTime * moveDir;
+        }
+
+        rb.AddForce(force, ForceMode.Force);
+
+        if(rb.velocity.y > 0){
+            rb.AddForce(Vector3.down * 80f, ForceMode.Force);
+        }
+    }
+    void ProcessSpriteFlip(){
         if (x != 0 && x < 0){
             sr.flipX=true;
         }
